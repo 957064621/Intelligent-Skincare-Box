40e3b906f139dd6c293bf1392bf21ae732c2cbe8
Yuko_lu <957064621@qq.com>
2023-05-18 12:44:37 +0800

    3

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.getElementById('container');
   const svg = document.getElementById('my-svg');
+  const loadingAnimation = document.getElementById('loading-animation');
+  svg.style.display = 'none';
+
+  svg.addEventListener('load', () => {
+    loadingAnimation.style.display = 'none';
+    svg.style.display = 'block';
+  }
 
   container.addEventListener('scroll', () => {
     svg.style.transform = `translateY(${container.scrollTop}px)`;
